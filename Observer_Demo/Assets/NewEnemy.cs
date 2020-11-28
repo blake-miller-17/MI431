7c63dff5ddcf04e8cc1804b11a19e24fbdc1e9d7
blake-miller-17 <bgarrettm@gmaail.com>
2020-11-27 19:41:57 -0500

    Added projects

---
+++ Observer_Demo/Assets/NewEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Grabs all the enemies with NewEnemy script, then allows them to be
+        //subscribed to that event, ready to do something
+		NewPlayer.EnemyChangeColor += ChangeColor;
+        //NewPlayer.EnemyChangeColor += takeDamage;
+    }
+
+	//Call this method every time we want to change the color
+    //We can have more than one Event for our EnemyColor delegate
+    //We can also have more than one method, but it has to take a Color
+	private void ChangeColor(Color color)
+	{
+		transform.GetComponent<Renderer>().material.color = color;
+	}
+
+    //memory leak can happen if you do not unsubscribe from the event
+    //This is in the case that an object is destroyed/ deleted
+	void OnDestroy()
+    {
+        NewPlayer.EnemyChangeColor -= ChangeColor;
+    }
+
+}
---
+++ Observer_Demo/Assets/NewPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : MonoBehaviour
+{
+    //#region
+
+    //#endregion
+    public delegate void EnemyColor(Color color);
+    public static event EnemyColor EnemyChangeColor; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+                      
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+		{
+            //Check to make sure there are subscribers
+            if (EnemyChangeColor != null)
+			{
+                EnemyChangeColor(Color.red);
+			}
+		}
+    }
+}
